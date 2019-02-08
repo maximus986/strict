2d821057ccb631966f608ed4c29abee956311a80
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2019-02-08 21:54:25 +0100

    Make hero section full height, improve animations and make iOS and android compatible

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -14,33 +14,85 @@ $(document).ready(function () {
 
   //Reduce header padding on scroll
   $(window).scroll(function () {
+    animation(); // Start animation on window scroll
+    animateHeader(); //Animate header on window scroll
+  });
+
+  animation(); // Start animation on window load
+  animateHeader(); //Animate header on window load
+
+  //Header animation function
+  function animateHeader() {
     const scroll = $(window).scrollTop();
     if (scroll > 150) {
       $(".main-header").removeClass("py-lg-4");
     } else {
       $(".main-header").addClass("py-lg-4");
     }
-  });
+  }
 
-  // Animations
+  // Animation function
 
   function animation() {
+
     const windowHeight = $(window).height();
-    const scrollTop = $(window).scrollTop();
+    const scrollDown = $(window).scrollTop();
 
     $('.animation').each(function () {
-      const topPosition = $(this).offset().top;
-      if (topPosition < scrollTop + windowHeight - 100) {
-        const animation = $(this).data('animation');
+      var position = $(this).offset().top;
+
+      if (position < scrollDown + windowHeight - 50) {
+        const animation = $(this).attr('data-animation');
+        const delay = $(this).attr('data-delay');
+        $(this).css('animation-delay', delay);
         $(this).addClass(animation);
       }
     });
   }
 
-  animation();
-
-  $(window).scroll(function () {
-    animation();
+  // Form validation
+  $(function () {
+    $(".subscribe-form").validate({
+      highlight: function (element) {
+        $(element).closest(".form-group").addClass("has-danger");
+        $(element).addClass("form-control-danger");
+      },
+      unhighlight: function (element) {
+        $(element).closest(".form-group").removeClass("has-danger").addClass("has-success");
+        $(element).removeClass("form-control-danger").addClass("form-control-success");
+      },
+      rules: {
+        name: {
+          required: true,
+          rangelength: [2, 20]
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        message: {
+          required: true,
+          maxlength: 255
+        }
+      },
+      messages: {
+        name: {
+          required: "This field is required.",
+          rangelength: 'This field must contain between 2 adn 20 characters.'
+        },
+        email: {
+          required: "This field is required.",
+          email: 'Please enter a valid email.'
+        },
+        message: {
+          required: "This field is required.",
+          maxlength: "This field can't have more than 255 characters."
+        }
+      },
+      errorElement: "p",
+      errorPlacement: function (error, element) {
+        error.appendTo($(element).closest(".form-group").find(".error-msg"));
+      }
+    });
   });
-
 });
